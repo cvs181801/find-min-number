1be0cba5fb028ada4c01078dd641058a45221853
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-01 16:53:55 -0700

    fix lesser than sign

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ array1 = [-5, -6, -9, -13];
 function findMin(nums) {
     let min = nums[0];
     for (let i=0; i< nums.length; i++ ) {
-        if (nums[i] -= min) {
+        if (nums[i] <= min) {
             min = nums[i];
         }
     }
